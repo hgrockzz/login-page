870b4e257a0a88e1395493e3046a8cd9fb47ada9
Harshit Gupta <hgtgt07@gmail.com>
2024-10-03 09:17:41 +0530

    routing basic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+//UI
+import "./App.css";
+
+//Functioning
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 
 function App() {
+  const isAuthenticated = () => {
+    return !!localStorage.getItem("token");
+  };
+  const PrivateRoute = ({ child }) => {
+    return isAuthenticated() ? child : <Navigate to="/login" />;
+  };
+
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <HomePage />
+            </PrivateRoute>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </Router>
   );
 }
 
